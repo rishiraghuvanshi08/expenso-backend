545e93ee08bf4d9e1d3b87b36e5dfd5605400fd0
rishiraghuvanshi08 <rishiraghuvanshi08@gmail.com>
2025-07-06 17:31:40 +0530

    chore(project-setup): initialize Spring Boot backend with basic structure
    Initialized via Spring Initializr with dependencies for web, JPA, and MySQL.

---
+++ src/test/java/com/expenso/Expenso/ExpensoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenso.Expenso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
